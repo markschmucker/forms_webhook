4130f58b8d853caa42abf7a6022e78d56f5b14f2
markschmucker <36678240+markschmucker@users.noreply.github.com>
2019-10-05 11:14:52 +0930

    8082

--- webhook.py
+++ webhook.py
@@ -146,4 +146,4 @@ def handler(groupname):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=8082, debug=True, threaded=True)
